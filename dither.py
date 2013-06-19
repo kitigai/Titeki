20e585455ffa3b617defbab74ad2e0877d0b575e
kitigai <kitigai_morita@yahoo.co.jp>
2013-06-19 09:19:54 +0900

    fix

--- dither.py
+++ dither.py
@@ -1,8 +1,10 @@
 #coding:utf-8
 import sys
 import numpy
+import cv2
 from pylab import *
 from pypgm import pgmdat
+import matplotlib.pyplot as plt
 
 argvs = sys.argv
 if (len(argvs) != 3):
@@ -13,6 +15,7 @@ imagefilename = argvs[1]
 savefilename = argvs[2]
 pgm = pgmdat()
 pgm.readimg(imagefilename)
+img = pgm.img
 width = pgm.width
 height = pgm.height
 
@@ -23,3 +26,7 @@ matrix2 = [[0,128,32,160],
 	   [192,64,224,96],
 	   [48,176,16,144],
 	   [240,112,208,80]]
+
+
+cv2.imshow("tes",img)
+cv2.waitKey(0)
