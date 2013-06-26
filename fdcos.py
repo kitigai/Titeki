2e73de05c211201a9252c2100c70b5f514f83350
kitigai <kitigai_morita@yahoo.co.jp>
2013-06-26 23:41:50 +0900

    fix

--- fdcos.py
+++ fdcos.py
@@ -48,7 +48,7 @@ def idct2(F):
 		for n in range(N):
 			for u in range(M):
 				for v in range(N):
-					f[m][n] += alp(u,N)*alp(v,N)*f[u][v]*np.cos(np.pi*u*(2*m+1)/(2*M))*np.cos(np.pi*v*(2*n+1)/(2*N))
+					f[m][n] += alp(u,N)*alp(v,N)*F[u][v]*np.cos(np.pi*u*(2*m+1)/(2*M))*np.cos(np.pi*v*(2*n+1)/(2*N))
 
 
 	return f
@@ -77,28 +77,11 @@ if __name__ == "__main__":
 	for h in range(0,height,8):
 		for w in range(0,width,8):
 			fix[h:h+8,w:w+8] = dct2(img[h:h+8,w:w+8])
-			#pre = deepcopy(img[h:h+8,w:w+8])
-			#for dh in range(8):
-			#	fix[h:h+8,w:w+8][dh] = dct(img[h:h+8,w:w+8][dh])
-			#pre = deepcopy(pre.T)
-			#for dw in range(8):
-			#	fix[h:h+8,w:w+8][dw] = dct(fix[h:h+8,w:w+8].T[dw])
-			#fix[h:h+8,w:w+8] = fix[h:h+8,w:w+8].T
-			#fix[h:h+8,w:w+8] = deepcopy(pre)
 
 
 	for h in range(0,height,8):
 		for w in range(0,width,8):
 			ifix[h:h+8,w:w+8] = idct2(fix[h:h+8,w:w+8])
-			#ipre = deepcopy(fix[h:h+8,w:w+8])
-			#for dh in range(8):
-			#	ifix[h:h+8,w:w+8][dh] = idct(fix[h:h+8,w:w+8].T[dh])
-			#pre = deepcopy(pre.T)
-			#for dw in range(8):
-			#	ifix[h:h+8,w:w+8][dw] = idct(ifix[h:h+8,w:w+8].T[dw])
-			#for dw in range(8):
-			#	pre[dw][:] = idct(pre[dw])
-			#ifix[h:h+8,w:w+8] = deepcopy(ipre)
 		
 	plt.imshow(ifix)
 	plt.gray()
