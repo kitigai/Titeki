cdb1c632cb66c0b19d5fe7408d8805620fee50a5
kitigai <kitigai_morita@yahoo.co.jp>
2013-07-18 00:43:59 +0900

    DWT

---
+++ DWT.py
@@ -0,0 +1,82 @@
+#coding:utf-8
+"""[課題1]ハールウエーブレット解析"""
+import sys
+import numpy as np
+from pylab import *
+from pypgm import pgmdat
+import copy
+import matplotlib.pyplot as plt
+
+
+def downscale(c0):
+	N = len(c0)
+	c1 = np.zeros(N/2)
+	d1 = np.zeros(N/2)
+	
+	for i in range(N/2):
+		c1[i] = (c0[2*i] + c0[2*i + 1]) / 2.0
+		d1[i] = (c0[2*i] - c0[2*i + 1]) / 2.0
+
+	return c1,d1
+
+def DWT(c):
+	N,M = c.shape
+	c1 = np.zeros([N,M])
+	for n in range(N):
+		cn,dn = downscale(c[n])
+		c1[n] = np.hstack((cn,dn))
+
+
+
+	c1 = c1.T
+
+	for m in range(M):
+		cn,dn = downscale(c1[m])
+		c1[m] = np.hstack((cn,dn))
+
+	c1 = c1.T
+	return c1
+	
+		
+			
+	
+def DWTloop(c, Nsize, Msize):
+	#c1 = np.zeros([Nsize,Msize])
+	c1 = c[0:Nsize,0:Msize]
+
+	c1 = DWT(c1)
+
+
+	c[0:Nsize,0:Msize] = c1
+	return c
+
+	
+	
+	
+
+if __name__ == "__main__":
+
+	argvs = sys.argv
+	if (len(argvs) != 3):
+		print "miss"
+		quit()
+
+	imagefilename = argvs[1]
+	pgm = pgmdat()
+	pgm.readimg(imagefilename)
+	img = pgm.img
+	width = pgm.width
+	height = pgm.height
+
+
+	level = int(argvs[2])
+	for i in range(level):
+		img = DWTloop(img,height,width)
+		height /= 2
+		width /= 2
+	#fix = np.array([[255]*width]*height)
+	plt.imshow(img)
+	plt.gray()
+	plt.show()
+	#cv2.imshow("tes",img)
+	#cv2.waitKey(0)
